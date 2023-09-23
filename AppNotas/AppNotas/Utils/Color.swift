22cd4ecc5544957145ad7c5b44bc7fce09074a17
Gabriel Mors <gabrielmors210@gmail.com>
2023-09-23 13:29:38 -0400

    criado file para colocar as cores do app

---
+++ AppNotas/AppNotas/Utils/Color.swift
@@ -0,0 +1,22 @@
+//
+//  Color.swift
+//  AppNotas
+//
+//  Created by Gabriel Mors  on 23/09/23.
+//
+
+import Foundation
+import SwiftUI
+
+class CustomColor {
+    
+    /// Cor rosa do aplicativo.
+    ///  - Hexadecimal: #e6007e
+    ///  - RGB: red: 230/255, green: 0/255, blue: 126/255
+    static let pinkColor: Color = Color(red: 230/255, green: 0/255, blue: 126/255)
+    
+    /// Cor padrão de fundo do aplicativo.
+    ///  - Hexadecimal: #11151e
+    ///  - RGB: red: 17/255, green: 21/255, blue: 30/255
+    static let backgroundColor: Color = Color(red: 17/255, green: 21/255, blue: 30/255)
+}
