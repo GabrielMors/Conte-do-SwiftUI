2922f42411b92d56c3041f67dce2f4a09c913fa0
Barbara Brigolin <barbarabrigolin@hotmail.com>
2023-09-23 17:26:37 -0300

    import Firebase

--- AppNotas/AppNotas/AppNotasApp.swift
+++ AppNotas/AppNotas/AppNotasApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct AppNotasApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             Login()
         }
     }
+}
+
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+      
+    return true
+  }
 }
