58371dcec1e75cb7fbc7aa629093ff28dc06ac07
Caio Fabrini <caiofabrini@outlook.com>
2023-09-24 09:34:10 -0300

    ajustes login

--- AppNotas/AppNotas/Login/Login.swift
+++ AppNotas/AppNotas/Login/Login.swift
@@ -14,72 +14,68 @@ struct Login: View {
     @State private var isLoginActive = false
     
     var body: some View {
-        ZStack {
-            CustomColor.backgroundColor.ignoresSafeArea()
-            VStack(spacing: 20) {
-                Text("Login")
-                    .font(.system(size: 55, weight: .bold))
-                    .foregroundColor(.white)
-                    .padding(.top, 10)
-                
-                TextField("E-mail", text: $email)
-                    .textFieldStyle(PlainTextFieldStyle())
-                    .foregroundColor(.white)
-                    .accentColor(.white)
-                    .frame(height: 40)
-                    .padding(EdgeInsets(top: 4, leading: 8, bottom: 4, trailing: 0))
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(CustomColor.pinkColor, lineWidth: 2)
-                    )
-                    .padding(.top, 70)
-                    .padding(.bottom, 20)
-                
-                SecureField("Password", text: $password)
-                    .textFieldStyle(PlainTextFieldStyle())
-                    .foregroundColor(.white)
-                    .accentColor(.white)
-                    .frame(height: 40)
-                    .padding(EdgeInsets(top: 4, leading: 8, bottom: 4, trailing: 0))
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(CustomColor.pinkColor, lineWidth: 2)
-                    )
-                    .padding(.bottom, 100)
-                
-                Spacer()
-                Button(action: {
-                    isLoginActive = true
-                }) {
+        NavigationStack {
+            ZStack {
+                CustomColor.backgroundColor.ignoresSafeArea()
+                VStack(spacing: 25) {
                     
                     Text("Login")
-                        .foregroundColor(.white)
-                        .font(.system(size: 17, weight: .bold))
-                        .frame(width: 130, height: 40)
-                        .background(CustomColor.pinkColor)
-                        .cornerRadius(8)
-                }
-                NavigationLink("", destination: NotesListView(), isActive: $isLoginActive)
-                              .opacity(0)
-                              .frame(width: 0, height: 0)
-                Spacer()
-                
-                Button(action: {
-                    isRegisterActive = true
-                }) {
+                        .font(.system(size: 55, weight: .bold))
+                        .foregroundStyle(.white)
+                        .padding(.top, 10)
                     
-                    Text("Don't have an account? Register")
-                        .foregroundColor(.white)
-                        .font(.system(size: 17, weight: .bold))
-                        .frame(height: 40)
+                    Spacer()
+                    
+                    Group {
+                        TextField("", text: $email, prompt: Text("E-mail").foregroundColor(.gray))
+                            .keyboardType(.emailAddress)
+                        SecureField("", text: $password, prompt: Text("Password").foregroundColor(.gray))
+                    }
+                    .frame(height: 40)
+                    .padding(7)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(CustomColor.pinkColor, lineWidth: 2)
+                    )
+                    .padding(7)
+                    .foregroundColor(.white)
+                    .accentColor(.white)
+                    .textInputAutocapitalization(.never)
+                    .autocorrectionDisabled()
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        isLoginActive = true
+                    }) {
+                        Text("Login")
+                            .foregroundColor(.white)
+                            .font(.system(size: 17, weight: .bold))
+                            .frame(width: 180, height: 45)
+                            .background(CustomColor.pinkColor)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        isRegisterActive.toggle()
+                    }) {
+                        Text("Don't have an account? Register")
+                            .foregroundColor(.white)
+                            .font(.system(size: 17, weight: .bold))
+                            .frame(height: 40)
+                    }
                 }
-                NavigationLink("", destination: Register(), isActive: $isRegisterActive)
-                              .opacity(0)
-                              .frame(width: 0, height: 0)
-                      
+                .padding(.horizontal, 20)
+                .padding(.bottom, 50)
             }
-            .padding(.horizontal, 20)
-            .padding(.bottom, 50)
+            .navigationDestination(isPresented: $isRegisterActive) {
+                Register()
+            }
+            .navigationDestination(isPresented: $isLoginActive) {
+                NotesListView()
+            }
         }
     }
 }
